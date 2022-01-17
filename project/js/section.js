063ec8efb40c8d75f67fa7590426d98b083a8281
AbdelrahmanEhab <57192912+AbdelrahmanEhab20@users.noreply.github.com>
2022-01-17 23:26:55 +0200

    first commit

---
+++ project/js/section.js
@@ -0,0 +1,9 @@
+let section = document.getElementsByClassName("hero01")[0]
+
+
+const element = document.createElement("h1");
+
+element.innerHTML = "This Is A Great Website + test Update";
+section.appendChild(element)
+
+// this is a comment
